a75f7765c7e9c9905abcb6494be2142a342e9436
Trubnikova-Mariya <trubnikova.mp@gmail.com>
2021-05-17 19:57:09 +0400

    Fix avatar group styles

--- src/components/AvatarGroup/AvatarGroup.jsx
+++ src/components/AvatarGroup/AvatarGroup.jsx
@@ -7,19 +7,31 @@ import { observer } from "mobx-react";
 const AvatarGroup = observer(({ task }) => {
   const { users } = usersStore.state;
 
-  let executors = task.users.filter(item => item.roles.includes('executor'));
-  let creators = task.users.filter(item => item.roles.includes('creator') && !executors.includes(item));
-  let commentators = task.users.filter(item => !creators.includes(item) && !executors.includes(item));
+  let executors = task.users.filter((item) => item.roles.includes("executor"));
+  let creators = task.users.filter(
+    (item) => item.roles.includes("creator") && !executors.includes(item)
+  );
+  let commentators = task.users.filter(
+    (item) => !creators.includes(item) && !executors.includes(item)
+  );
 
   let sorted = executors.concat(creators).concat(commentators);
 
-  return <Avatar.Group maxCount={4}>
+  return (
+    <Avatar.Group
+      maxCount={4}
+      size="small"
+      maxStyle={{
+        backgroundColor: "#6FCF97",
+        fontSize: "9px",
+      }}
+    >
       {sorted.map((item) => {
         item = users.find((user) => user.id == item.id);
-        return <UserAvatar key={item.id} user={item} />
+        return <UserAvatar key={item.id} user={item} />;
       })}
-      
-  </Avatar.Group>;
+    </Avatar.Group>
+  );
 });
 
 export default AvatarGroup;
